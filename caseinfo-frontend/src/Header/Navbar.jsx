15ebab521e8e10fb287a23af60ffd62e4e810939
yasir027 <116998637+yasir027@users.noreply.github.com>
2024-05-25 19:35:33 +0530

    Update Navbar.jsx

--- caseinfo-frontend/src/Header/Navbar.jsx
+++ caseinfo-frontend/src/Header/Navbar.jsx
@@ -1,28 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 import styles from "./Navbar.module.css";
 
-const Navbar = ({ user, handleSignOut }) => {
+const Navbar = ({ handleSignOut }) => {
   return (
     <div className={styles.navBarDefault}>
       <div className={styles.frame}>
-        <Link to="/recycle-bin" className={styles.navButton}>
+        <NavLink to="/recycle-bin" className={styles.navButton}>
           Recycle Bin
-        </Link>
-        <Link to="/reports" className={styles.navButton}>
+        </NavLink>
+        <NavLink to="/reports" className={styles.navButton}>
           Reports
-        </Link>
-        <Link to="/user-privileges" className={styles.navButton}>
+        </NavLink>
+        <NavLink to="/user-privileges" className={styles.navButton}>
           User Privileges
-        </Link>
-        <Link to="/profile" className={styles.navButton}>
+        </NavLink>
+        <NavLink to="/profile" className={styles.navButton}>
           Profile Settings
-        </Link>
-        {user && (
-          <button onClick={handleSignOut} className={styles.navButton}>
-            Logout
-          </button>
-        )}
+        </NavLink>
+        <button onClick={handleSignOut} className={styles.navButton}>
+          Logout
+        </button>
       </div>
     </div>
   );
