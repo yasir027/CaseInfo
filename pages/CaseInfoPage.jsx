f0b8dc1e0be93d964a837b6e11dde8387b12987a
yasir027 <116998637+yasir027@users.noreply.github.com>
2024-07-09 18:59:47 +0530

    Add files via upload

---
+++ pages/CaseInfoPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../CaseInfo/Form";
+const CaseInfoPage = () => {
+   return (
+      <>
+         <div>
+            <Form />
+         </div>
+      </>
+   );
+};
+
+export default CaseInfoPage;
